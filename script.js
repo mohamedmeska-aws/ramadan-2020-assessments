ee5beedf29d57415722d035c3fdccbf5ad9f953e
mohamedmeska-aws <mohamedmostafa@awstreams.com>
2020-04-29 23:26:25 +0200

    UPDATE show_video_requests_list

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+function singleVidReq(vidReqInfo){
+	let vidReqTemplate = document.createElement('div');
+	vidReqTemplate.innerHTML = `
+		<div class="card mb-3">
+			<div class="card-body d-flex justify-content-between flex-row">
+				<div class="d-flex flex-column">
+					<h3>${vidReqInfo.topic_title}</h3>
+					<p class="text-muted mb-2">${vidReqInfo.topic_details}</p>
+					<p class="mb-0 text-muted">
+						<strong>Expected results:</strong> ${vidReqInfo.expected_result}
+					</p>
+				</div>
+				<div class="d-flex flex-column text-center">
+					<a class="btn btn-link">🔺</a>
+					<h3>0</h3>
+					<a class="btn btn-link">🔻</a>
+				</div>
+			</div>
+			<div class="card-footer d-flex flex-row justify-content-between">
+				<div>
+					<span class="text-info">${vidReqInfo.status.toUpperCase()}</span> &bullet; added by <strong>${vidReqInfo.author_name}</strong> on <strong>${new Date(vidReqInfo.submit_date).toLocaleDateString()}</strong>
+				</div>
+				<div class="d-flex justify-content-center flex-column 408ml-auto mr-2">
+					<div class="badge badge-success">${vidReqInfo.target_level}</div>
+				</div>
+			</div>
+		</div>
+	`;
+
+	return vidReqTemplate;
+}
+
 document.addEventListener('DOMContentLoaded', function(){
 	const reqVidForm = document.getElementById('req_vid_form');
 
@@ -10,6 +42,21 @@ document.addEventListener('DOMContentLoaded', function(){
 			method: 'POST',
 			body: {reqVidFormData}
 		})
+		.then(blob => blob.json())
 		.then(res => console.table(res))
+	});
+
+	
+
+	let vidRequets = document.getElementById('list_of_requests');
+
+	fetch('http://localhost:7777/video-request', {
+		method: 'GET'
+	})
+	.then(blob => blob.json())
+	.then(res => {
+		res.forEach(vidReqInfo => {
+			vidRequets.appendChild(singleVidReq(vidReqInfo));
+		});
 	});
 });
